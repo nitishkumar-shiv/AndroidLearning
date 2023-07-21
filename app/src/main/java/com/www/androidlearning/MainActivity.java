19f5ea5970e18ea451c4c05ed2ce22bfc899fa83
nitishkumar-shiv <>
2023-07-21 12:28:07 +0530

    counter app

--- app/src/main/java/com/www/androidlearning/MainActivity.java
+++ app/src/main/java/com/www/androidlearning/MainActivity.java
@@ -3,12 +3,39 @@ package com.www.androidlearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button inc,dec;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.counterView);
+        dec = findViewById(R.id.decButton);
+        inc = findViewById(R.id.incButton);
+
+        dec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter = Integer.parseInt(textView.getText().toString());
+                counter--;
+                textView.setText(Integer.toString(counter));
+            }
+        });
+
+        inc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter = Integer.parseInt(textView.getText().toString());
+                counter++;
+                textView.setText(Integer.toString(counter));
+            }
+        });
     }
 }
